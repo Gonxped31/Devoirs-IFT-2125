b66a0e17f4e7acde7752c349d1f1c3d800c3ea6f
Gonxped31 <115489183+Gonxped31@users.noreply.github.com>
2024-04-21 15:37:06 -0400

    Finished Q_1

--- Devoir_4/code/Q2 well_placement/well_placement.py
+++ Devoir_4/code/Q2 well_placement/well_placement.py
@@ -1,46 +1,56 @@
-# Nom, Matricule
-# Nom, Matricule
+# Gbian Bio Samir, 20250793
+# Sourou Johann, 20227958
 
 import sys
 
-def read_problem(MyGraph, input_file="input.txt"):
-    """Fonctions pour lire/écrire dans les fichier. Vous pouvez les modifier,
-    faire du parsing, rajouter une valeur de retour, mais n'utilisez pas
-    d'autres librairies.
-    Functions to read/write in files. you can modify them, do some parsing,
-    add a return value, but don't use other librairies"""
-
-    # lecture du fichier/file reading
-    file = open(input_file, "r")
-    lines = file.readlines()
-    file.close()
-
-    # TODO: Compléter ici/Complete here
-    # traiter les lignes du fichier pour le problème
-    # process the file lines for the problem
-    
-
+def read_problem(input_file="input.txt"):
+    with open(input_file, "r") as file:
+        lines = file.readlines()
+    graph = [list(map(int, line.strip())) for line in lines[1:]]
+    return graph
 
 def write(fileName, content):
-    """Écrire la sortie dans un fichier/write output in file"""
-    file = open(fileName, "w")
-    file.write(content)
-    file.close()
+    with open(fileName, "w") as file:
+        file.write(content)
 
+def dfs(graph, start_i, start_j, visited_nodes):
+    stack = [(start_i, start_j)]
+    count = 0
+
+    while stack:
+        i, j = stack.pop()
+        if i < 0 or i >= len(graph) or j < 0 or j >= len(graph[0]):
+            continue
+        if visited_nodes[i][j] or graph[i][j] != 1:
+            continue
+
+        visited_nodes[i][j] = True
+        count += 1
+
+        stack.append((i-1, j))
+        stack.append((i+1, j))
+        stack.append((i, j-1))
+        stack.append((i, j+1))
+
+    return count
 
 def main(args):
-    """Fonction main/Main function"""
     input_file = args[0]
     output_file = args[1]
+    graph = read_problem(input_file)
+    rows, cols = len(graph), len(graph[0])
+    visited_nodes = [[False] * cols for _ in range(rows)]
+    best_well = 0
 
-    # TODO : Compléter ici/Complete here...
-    # Vous pouvez découper votre code en d'autres fonctions...
-    # You may split your code in other functions...
+    for i in range(rows):
+        for j in range(cols):
+            if graph[i][j] == 1 and not visited_nodes[i][j]:
+                best_well = max(best_well, dfs(graph, i, j, visited_nodes))
 
-    answer = 0
+    answer = best_well
 
-    # answering
     write(output_file, str(answer))
+
 
 # NE PAS TOUCHER
 # DO NOT TOUCH
